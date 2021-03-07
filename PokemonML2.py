f35cc46632ae723c7b5016c3999b4bf8cfcd8ca1
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-03-07 13:58:03 +0100

    Final analysis about types performances

--- PokemonML2.py
+++ PokemonML2.py
@@ -1,12 +1,10 @@
-# Etude des victoires en fonction du type
+# Etude des victoires en fonction du type 1 et 2
 
 # Import des modules
 
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 pd.set_option('expand_frame_repr', False)
 
 # Recopie du script PokemonML1
@@ -32,4 +30,25 @@ Pokemons['TYPE_1_ET_2'] = Pokemons.TYPE_1_ET_2.str.replace(r'\bet $', '', regex=
 print(Pokemons.head(10))
 
 Combats = pd.read_csv("datas/combats.csv")
+
+Combats['Premier_Pokemon'] = Combats['Premier_Pokemon'].map(Pokemons.set_index('NUMERO')['TYPE_1_ET_2'])
+Combats['Second_Pokemon'] = Combats['Second_Pokemon'].map(Pokemons.set_index('NUMERO')['TYPE_1_ET_2'])
+Combats['Pokemon_Gagnant'] = Combats['Pokemon_Gagnant'].map(Pokemons.set_index('NUMERO')['TYPE_1_ET_2'])
 print(Combats.head(10))
+
+premiere_position = Combats.groupby('Premier_Pokemon').count()
+seconde_position = Combats.groupby('Second_Pokemon').count()
+
+victoires = Combats.groupby('Pokemon_Gagnant').count()
+
+list_agreg = Combats.groupby('Pokemon_Gagnant').count()
+list_agreg.sort_index()
+
+list_agreg['NBR_COMBATS'] = premiere_position.Pokemon_Gagnant + seconde_position.Pokemon_Gagnant
+list_agreg['NBR_VICTOIRES'] = victoires.Premier_Pokemon
+
+list_agreg['POURCENTAGE_VICTOIRES'] = victoires.Premier_Pokemon / (premiere_position.Pokemon_Gagnant
+                                                                   + seconde_position.Pokemon_Gagnant)
+list_agreg = list_agreg.drop(['Premier_Pokemon', 'Second_Pokemon'], axis=1)
+print(list_agreg.groupby('Pokemon_Gagnant').agg({"POURCENTAGE_VICTOIRES": "mean"})
+      .sort_values(by="POURCENTAGE_VICTOIRES", ascending=False).head(30))
